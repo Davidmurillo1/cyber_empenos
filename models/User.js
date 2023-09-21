48e26f07a2e40213159b557a44bfd841e474da14
david_murillo1 <124529219+Davidmurillo1@users.noreply.github.com>
2023-09-20 20:10:12 -0600

    Quitar el console.log de sucursales

--- models/User.js
+++ models/User.js
@@ -43,7 +43,7 @@ class User {
             }
         }
     
-        console.log(`Sucursales para el usuario ${userId}:`, sucursales); // Agregamos un log aquí
+        // console.log(`Sucursales para el usuario ${userId}:`, sucursales); // Agregamos un log aquí
     
         return sucursales;
     }
