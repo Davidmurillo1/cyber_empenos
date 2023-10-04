d4e362b7035b61769bcb44c99b248c58b3dc8473
david_murillo1 <124529219+Davidmurillo1@users.noreply.github.com>
2023-10-04 00:23:54 -0600

    Se solucionó un problema en el db.js

--- models/db.js
+++ models/db.js
@@ -2,12 +2,13 @@ const mysql = require('mysql2');
 const { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT } = require('../config');
 
 const pool = mysql.createPool({
-    host: 'DB_HOST',
-    user: 'DB_USER',
-    password: 'DB_PASSWORD',
-    port: 'DB_PORT',
-    database: 'DB_NAME'
+    host: DB_HOST,  // Aquí
+    user: DB_USER,  // Aquí
+    password: DB_PASSWORD,  // Aquí
+    port: DB_PORT,  // Aquí
+    database: DB_NAME  // Y aquí
 });
 
 module.exports = pool.promise();
+
 
