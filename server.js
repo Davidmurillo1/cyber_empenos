dcbbb3bd36c4cc405f73ad5038270a17b34aecbd
david_murillo1 <124529219+Davidmurillo1@users.noreply.github.com>
2023-10-04 17:50:01 -0600

    Se modificó el server.app para solucionar el session

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const session = require('express-session');
+const MySQLStore = require('express-mysql-session')(session);
 const flash = require('connect-flash');
 const authRoutes = require('./routes/authRoutes');
 const clientesRoutes = require('./routes/clientesRoutes');
@@ -9,41 +10,47 @@ const pagosRoutes = require('./routes/pagosRoutes');
 const printRoutes = require('./routes/printRoutes');
 const { PORT } = require('./config.js');
 
-
-
 const app = express();
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));  // Añadir esta línea para servir archivos estáticos
+app.use(express.static('public'));
+
+// Configuración de la tienda de sesiones de MySQL
+const sessionStore = new MySQLStore({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    // ... otras opciones si las necesitas
+});
 
 app.use(session({
     secret: 'tu_clave_secreta',
     resave: false,
-    saveUninitialized: false
+    saveUninitialized: false,
+    store: sessionStore  // Usa MySQL para almacenar las sesiones
 }));
 
 app.use((req, res, next) => {
-    if (!req.session.role && req.path !== '/login') { // Excluye la ruta de login
+    if (!req.session.role && req.path !== '/login') {
         return res.redirect('/login');
     }
-    if (req.session && req.session.user) { // Verifica si el usuario está en la sesión
-        res.locals.userName = req.session.user.nombre; // Pasa el nombre del usuario a todas las vistas
+    if (req.session && req.session.user) {
+        res.locals.userName = req.session.user.nombre;
     }
     res.locals.role = req.session.role;
     next();
 });
 
 app.use(async (req, res, next) => {
-    // Verifica si hay una sucursal en la sesión
     if(req.session && req.session.sucursal) {
         try {
             // Aquí debes poner el código necesario para cargar el logo de la base de datos
             // Este es solo un ejemplo y debes adaptarlo a tu implementación
             // Suponiendo que tienes un método para obtener el logo por el id de la sucursal
             const logo = await Sucursal.obtenerLogoPorId(req.session.sucursal.id);
-            
-            // Almacenar el logo en res.locals para que esté disponible en las vistas
             res.locals.sucursalLogo = logo;
         } catch (error) {
             console.error('Error al obtener el logo de la sucursal:', error);
@@ -51,8 +58,6 @@ app.use(async (req, res, next) => {
     }
     next();
 });
-
-
 
 app.use(flash());
 app.use((req, res, next) => {
@@ -67,7 +72,6 @@ app.use(empenosRoutes);
 app.use(joyasRoutes);
 app.use(pagosRoutes);
 app.use(printRoutes);
-
 
 app.listen(PORT, () => {
     console.log(`Servidor escuchando en el puerto ${PORT}`);
